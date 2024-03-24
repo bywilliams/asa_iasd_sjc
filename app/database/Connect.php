36bad3a120a169ee083e81355366f8480c229e01
bywilliams <willian.seu@gmail.com>
2024-03-24 18:38:57 -0300

    1.06 - Docker inserido no projeto

--- app/database/Connect.php
+++ app/database/Connect.php
@@ -4,15 +4,17 @@ namespace app\database;
 
 use PDO;
 use PDOException;
+use app\traits\SessionMessageTrait;
 
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 
-define('HOST', 'localhost');
-define('USER', 'root');
+define('HOST', 'mysql');
+define('USER', 'wil');
 define('DBNAME', 'asa_sjc');
-define('PASSWORD', '');
+define('PASSWORD', 'root');
+
 
 /**
  * Classe Connect
@@ -21,6 +23,8 @@ define('PASSWORD', '');
  */
 class Connect
 {
+    use SessionMessageTrait;
+
     protected $connection;
 
     public function __construct()
@@ -37,18 +41,18 @@ class Connect
      */
     public function connectDatabase()
     {
-
         date_default_timezone_set('America/Sao_Paulo');
         
         try {
-            $conn = 'mysql:unix_socket=/opt/lampp/var/mysql/mysql.sock;host=' . HOST . ';dbname=' . DBNAME;
+            $conn = 'mysql:host=' . HOST . ';port=3306;dbname=' . DBNAME; // Alterado para porta 8002
             $this->connection = new PDO($conn, USER, PASSWORD, [
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
             ]);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            echo "Error: " . $e->getMessage();
+            $this->log_error("Erro ao efetuar conexão com banco de dados: " . $e->getMessage());
             die();
         }
     }
+
 }
